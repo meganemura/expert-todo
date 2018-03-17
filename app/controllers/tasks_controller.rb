4adfdd26cbc89ee40deb5b200e5af938bdb48e39
meganemura <meganemura@users.noreply.github.com>
2018-03-17 14:18:09 +0900

    Implement TasksController{index,new,create}

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :due_date)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :link_authentications
+  has_many :tasks
 end
