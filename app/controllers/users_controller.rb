a85fce16c43c034d57090e3f042d58f34e07c029
meganemura <meganemura@users.noreply.github.com>
2018-03-17 14:58:00 +0900

    Add uniqueness validation to link_authentications.email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
   def create
     user = User.new
     user.link_authentications.build(email: user_params[:email], volatile_token: SecureRandom.uuid)
-    user.save
-    session[:user] = { id: user.id }
-    redirect_to me_path
+    if user.save
+      session[:user] = { id: user.id }
+      redirect_to me_path
+    else
+      redirect_to root_path
+    end
   end
 
   def show
