670c03b63d0135f5f71caace98031cde529ec136
meganemura <meganemura@users.noreply.github.com>
2018-03-17 14:04:42 +0900

    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_17_041602) do
+ActiveRecord::Schema.define(version: 2018_03_17_050333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -26,10 +26,20 @@ ActiveRecord::Schema.define(version: 2018_03_17_041602) do
     t.index ["user_id"], name: "index_link_authentications_on_user_id"
   end
 
+  create_table "tasks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id"
+    t.string "title"
+    t.date "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "link_authentications", "users"
+  add_foreign_key "tasks", "users"
 end
