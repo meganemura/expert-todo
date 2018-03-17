881f84bac3f5a3bc05bb15df9841bdafd321d94f
meganemura <meganemura@users.noreply.github.com>
2018-03-17 13:34:05 +0900

    Implement user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new
+    user.link_authentications.build(email: "#{SecureRandom.uuid}@example.com", volatile_token: SecureRandom.uuid)
+    user.save
+    session[:user] = { id: user.id }
+    redirect_to me_path
+  end
+
+  def show
+  end
 end
