33698d1d90f28850725bf35e393fa6469935487b
meganemura <meganemura@users.noreply.github.com>
2018-03-18 03:59:53 +0900

    authentication_tokens.email is unique

--- db/migrate/20180317185230_create_authentication_tokens.rb
+++ db/migrate/20180317185230_create_authentication_tokens.rb
@@ -2,7 +2,7 @@ class CreateAuthenticationTokens < ActiveRecord::Migration[5.2]
   def change
     create_table :authentication_tokens, id: :uuid do |t|
       t.references :user, foreign_key: true, type: :uuid
-      t.string :email
+      t.string :email, index: { unique: true }
       t.string :token
 
       t.timestamps
