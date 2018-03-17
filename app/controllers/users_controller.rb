a6a92bb2470562cabb98a72688b9f9da78fb094d
meganemura <meganemura@users.noreply.github.com>
2018-03-18 04:09:07 +0900

    Remove building link_authentications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new
-    user.link_authentications.build(email: user_params[:email], volatile_token: SecureRandom.uuid)
     user.build_authentication_token(email: user_params[:email])
     if user.save
       session[:user] = { id: user.id }
