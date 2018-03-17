53f190160f374456b4fe3474ce2cc6a5397fb453
meganemura <meganemura@users.noreply.github.com>
2018-03-17 14:52:21 +0900

    Sign up with email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
 
   def create
     user = User.new
-    user.link_authentications.build(email: "#{SecureRandom.uuid}@example.com", volatile_token: SecureRandom.uuid)
+    user.link_authentications.build(email: user_params[:email], volatile_token: SecureRandom.uuid)
     user.save
     session[:user] = { id: user.id }
     redirect_to me_path
   end
 
   def show
+  end
+
+  def user_params
+    params.permit(:email)
   end
 end
