c2b7a8d007fd8b581fbb01cf0300fd10f0679d49
meganemura <meganemura@users.noreply.github.com>
2018-03-17 14:04:29 +0900

    Specify type of id

--- db/migrate/20180317050333_create_tasks.rb
+++ db/migrate/20180317050333_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks, id: :uuid do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, type: :uuid
       t.string :title
       t.date :due_date
 
