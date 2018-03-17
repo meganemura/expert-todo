79a6f508592fe14eb7c3679ce913a30ef375ebe5
meganemura <meganemura@users.noreply.github.com>
2018-03-17 13:14:14 +0900

    Define SessionsController#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  def create
+    if link_authentication_params
+    end
+
+    redirect_to root_path
+  end
+
+  def link_authentication_params
+    params.permit(:x)
+  end
 end
