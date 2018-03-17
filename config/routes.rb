4caa6e7e996c5f9797dd38a6b7b4c975662ce96a
meganemura <meganemura@users.noreply.github.com>
2018-03-17 12:57:01 +0900

    Route top_pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'top_pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top_pages#index'
 end
