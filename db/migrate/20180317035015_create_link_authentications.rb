e2a299cbcef17ed933b3f9e2fe5b089893c0abae
meganemura <meganemura@users.noreply.github.com>
2018-03-17 12:51:10 +0900

    Define link_authentications table

--- db/migrate/20180317035015_create_link_authentications.rb
+++ db/migrate/20180317035015_create_link_authentications.rb
@@ -1,8 +1,8 @@
 class CreateLinkAuthentications < ActiveRecord::Migration[5.2]
   def change
     create_table :link_authentications, id: :uuid do |t|
-      t.references :user, foreign_key: true
-      t.string :email
+      t.references :user, foreign_key: true, type: :uuid
+      t.string :email, index: { unique: true }
 
       t.timestamps
     end
