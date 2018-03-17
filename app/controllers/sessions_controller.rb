4eec1fee8c68d27f53a59f62d3a1826ff161d707
meganemura <meganemura@users.noreply.github.com>
2018-03-17 13:56:27 +0900

    Redirect to /me after logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
       auth = LinkAuthentication.find_by(volatile_token: link_authentication_params[:x])
       if auth
         session[:user] = { id: auth.user_id }
+
         # TODO: destroy auth
+
+        return redirect_to me_path
       end
     end
 
