62de87fbf667ae19a5da3fa19cb9ec4926bae2c2
meganemura <meganemura@users.noreply.github.com>
2018-03-17 13:01:47 +0900

    Define ApplicationController#ensure_logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_logged_in?, unless: :public_page?
+
+  def ensure_logged_in?
+    redirect_to root_path
+  end
+
+  def public_page?
+    true
+  end
 end
